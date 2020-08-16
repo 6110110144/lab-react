de8222ab1498b242df9f3d3a158e983662590832
6110110144 <55184541+6110110144@users.noreply.github.com>
2020-08-16 20:53:11 +0700

    Reset and Count Attempt

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,16 +1,17 @@
 import React, {useState, useEffect,useRef} from 'react';
 export default function CharacterCard(props){
-    const attemptRef = useRef(props.attempt);
+    
     const [active, setActive] = useState(false);
+    const attemptRef = useRef(props.attempt);
+
     const activate = () => {
         setActive(true)
         props.activationHandler(props.value)
     }
     useEffect(() => {
         if(attemptRef.current != props.attempt){
-            setActive(false)
+            setActive(false);
             attemptRef.current = props.attempt
-
         }
     })
     const className = `card ${active ? 'activeCard': ''}`
